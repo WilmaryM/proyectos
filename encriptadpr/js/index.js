8b1ebc7c9891bdbb6a7534e669ef9db4b7f56c0d
wilmary <wilmarymercedes1404@gmail.com>
2023-10-24 11:48:58 -0400

    carpeta de practicas

---
+++ encriptadpr/js/index.js
@@ -0,0 +1,30 @@
+/*
+const cuadroTexto = document.getElementById('ocultar').style.visibility = "hidden";
+
+function encriptar() {
+    
+}
+*/
+
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let div = document.getElementById("ocultar");
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
+
+        if (texto.length != 0) {
+            document.getElementById("texto").value = textoCifrado;
+            div.textContent = document.getElementById("texto").value;
+            div.style.visibility = "hidden";
+
+        }else{
+            div.textContent = " ";
+            div.style.visibility = "visible";
+        }
+}
+
+
