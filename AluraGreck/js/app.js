3ae57d0b1a99fe3ef38824ed7967c26de416b086
Wilmary <107276432+WilmaryM@users.noreply.github.com>
2024-07-01 18:44:23 -0400

    Delete AluraGreck directory

--- AluraGreck/js/app.js
+++
@@ -1,7 +0,0 @@
-const btnLupa = document.querySelector('.btnLupa');
-const buscar = document.querySelector('.buscar');
-
-btnLupa.addEventListener('click', function() {
-
-        buscar.classList.toggle("buscar--show")
-})
